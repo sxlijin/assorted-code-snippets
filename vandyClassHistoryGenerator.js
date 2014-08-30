65294b7a5ae1da9796c5f816f3005ffab4974f20
Samuel Lijin <sxlijin@gmail.com>
2014-08-30 14:36:30 -0500

    Div generation & content done
    Prints semesters and classes to a div that it tosses in at the top. CSS
    styling for this div is also done now.
    
    The following still needs to be done: actually insert the stylesheet for
    the new stuff, optionally, append even and odd classes to the divs for
    each class so that they alternate color, grab the person's name and add
    it to the top

--- vandyClassHistoryGenerator.js
+++ vandyClassHistoryGenerator.js
@@ -1,21 +1,19 @@
-semesterTrees = document.getElementsByClassName('careerTermTable');
-
 function cleanString(messy) {
-	messy = messy.replace(/ +(?= )/g,'');
-	messy = messy.replace(/- +/, '-');
+	var messy = messy.replace(/ +(?= )/g,''),
+	    messy = messy.replace(/- +/, '-');
 	return messy;
 }
 
 function grabNthParent(orphan, nthParent) {
-	child = orphan;
-	for (j = 0; j < nthParent; j++) 
+	var child = orphan;
+	for (var i = 0; i < nthParent; i++) 
 		child = child.parentNode;
 	return child;
 }
 
 function getSemesterTitle(classesTable) {
-	divBigDaddy = grabNthParent(classesTable, 5);
-	divBigDaddy = divBigDaddy.children[0].children[0];
+	var divBigDaddy = grabNthParent(classesTable, 5),
+	    divBigDaddy = divBigDaddy.children[0].children[0];
 	return divBigDaddy.children[1].innerHTML.trim();
 }
 
@@ -24,22 +22,88 @@ function getClasses(tbodyClassesParent) {
 }
 
 function getClassInfo(trClassParent) {
-	classCode = trClassParent.children[0].innerHTML.trim();
-	className = trClassParent.children[1].innerHTML.trim();
-	classProf = trClassParent.children[2].children[0].innerHTML.trim();
+	var classCode = trClassParent.children[0].innerHTML.trim(),
+	    className = trClassParent.children[1].innerHTML.trim(),
+	    classProf = trClassParent.children[2].children[0].innerHTML.trim();
 	return [cleanString(classCode), cleanString(className), cleanString(classProf)];
 }
 
-for (i = 0; i < semesterTrees.length; i++) {
-	//console.log('loop '+i)
-	semester = semesterTrees[i]
-	semesterTitle = getSemesterTitle(semester);
-	classList = getClasses(semester);
-	console.log('\n'+semesterTitle);
-	for (k = 0; k < classList.length; k++) {
-		classInfo = getClassInfo(classList[k]);
-		
-		console.log(classInfo[0]+'    \t'+classInfo[1]+'\t'+classInfo[2]);
-	}
-	//console.log(classList.length)
+function createHTMLstr(htmlStr) {
+    var frag = document.createDocumentFragment(),
+        temp = document.createElement('div');
+    temp.innerHTML = htmlStr;
+    while (temp.firstChild) {
+        frag.appendChild(temp.firstChild);
+    }
+    return frag;
 }
+
+function dumpClassInfoToDivRow(classInfoArray) {
+	// Arg in the form [ classCode, className, classProf ], like so:
+	// ["MATH-205A-01", "Multivar Calc/Linear Alg", "Bruce Hughes"]
+	var classCodeDiv = '<div class="classCode classInfoElem">'+classInfoArray[0]+'</div>',
+	    classNameDiv = '<div class="className classInfoElem">'+classInfoArray[1]+'</div>',
+	    classProfDiv = '<div class="classProf classInfoElem">'+classInfoArray[2]+'</div>',
+	    classInfoDiv = '<div class="classInfoRow">';
+	    classInfoDiv += classCodeDiv+classNameDiv+classProfDiv;
+	    classInfoDiv += '</div>';
+	return classInfoDiv;
+}
+
+function dumpSemesterToDivBlock(semesterTree) {
+	var semesterTitle = getSemesterTitle(semesterTree),
+	    semesterTitleDiv = '<div class="semesterTitle">'+semesterTitle+'</div>',
+	    semesterDivBlockContent = '',
+	    classList = getClasses(semesterTree);
+	//console.log('\n'+semesterTitleDiv);
+	for (var k = 0; k < classList.length; k++) {
+		var classInfo = getClassInfo(classList[k]);
+		semesterDivBlockContent += dumpClassInfoToDivRow(classInfo);
+		//console.log(dumpClassInfoToDivRow(classInfo));
+	}
+	var semesterDivBlock = '<div class="semesterBlock">';
+	semesterDivBlock += semesterTitleDiv;
+	semesterDivBlock += semesterDivBlockContent;
+	semesterDivBlock += '</div>';
+	return semesterDivBlock;
+}
+
+function dumpSemestersToHTML() {
+	var semesterTrees = document.getElementsByClassName('careerTermTable'),
+	    hd = '<div class="roundedDialog"><div class="hd"></div>',
+     	    bd = genBody(),
+     	    ft ='<div class="ft"></div></div>';
+     	function genBody() {
+     		var bdHead = '<div class="bd"><div>someClasses</div>',
+		    bdBody = '',
+     		    bdFoot = '</div>';
+		for (var i = 0; i < semesterTrees.length; i++) {
+			var semester = semesterTrees[i];
+			bdBody += dumpSemesterToDivBlock(semester);
+		}
+     		return bdHead + bdBody + bdFoot;
+     	};
+     	return hd+bd+ft;
+}
+
+htmlDump = createHTMLstr(dumpSemestersToHTML());
+pageContent = document.getElementById('academicDetailTabContent_content');
+pageContent.insertBefore(htmlDump, pageContent.children[0]);
+
+
+/* CSS is below:
+.semesterBlock {margin:10px;}
+
+.classInfoRow{
+    margin:2px;
+    width:100%;
+}
+
+.classInfoElem {
+    display:inline-block;
+}
+
+.classCode {width:15%;}
+.className {width:45%;}
+.classProf {width:40%;}
+*/
