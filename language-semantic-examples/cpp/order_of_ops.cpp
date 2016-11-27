38c62dc5250200beb77697dc5b8ef195b012a940
Samuel Lijin <sxlijin@gmail.com>
2016-11-27 00:33:12 -0600

    add details about behavior and toolchain

--- language-semantic-examples/cpp/order_of_ops.cpp
+++ language-semantic-examples/cpp/order_of_ops.cpp
@@ -3,9 +3,31 @@
 
 /**
  *
- * I do different things when compiled with GCC and Clang!
+ * I do different things when compiled with GCC and Clang! Details below:
+ *
+ * $ uname -a
+ * Linux x220-arch-x64 4.8.10-1-ARCH #1 SMP PREEMPT Mon Nov 21 11:55:43 CET 2016 x86_64 GNU/Linux
+ *
+ * $ g++ --version
+ * g++ (GCC) 6.2.1 20160830
+ * Copyright (C) 2016 Free Software Foundation, Inc.
+ * This is free software; see the source for copying conditions.  There is NO
+ * warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * $ clang++ --version
+ * clang version 3.9.0 (tags/RELEASE_390/final)
+ * Target: x86_64-unknown-linux-gnu
+ * Thread model: posix
+ * InstalledDir: /usr/bin
+ *
+ * $ g++ order_of_ops.cpp -o a.out && ./a.out; echo $?
+ * 27
+ *
+ * $ clang++ order_of_ops.cpp -o a.out && ./a.out; echo $?
+ * 81
  *
  */
+
 
 struct Scalar {
     int data;
